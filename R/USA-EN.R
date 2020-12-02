e04e69dce70922c19c2ec72dcc1743bfba21d16b
Tu Hu <UYHDK@leo-pharma.com>
2020-12-02 19:54:00 +0100

    update

--- R/USA-EN.R
+++ R/USA-EN.R
@@ -9,27 +9,27 @@ library(purrr)
 
 extract_content <- function(url = ...){
   content <- url %>% xml2::read_html() %>% 
-    rvest::html_nodes(css ="#News_Body_Txt_A p") %>% rvest::html_text() %>% str_remove_all(BLANK) %>% 
+    rvest::html_nodes(css ="#News_Body_Txt_A p") %>% rvest::html_text()  %>% 
     str_subset("") %>% str_c(collapse = TRUE)
   return(content)
 }
 html <- xml2::read_html("http://www.china-embassy.org/eng/notices/")
 
 page <- tibble(Title = rvest::html_nodes(html, css = "#docMore a") %>% rvest::html_text() %>% 
-                 as.character() %>% str_replace_all(BLANK, ""),
+                 as.character(),
                Time = rvest::html_nodes(html,css = "#docMore li") %>% as.character() %>% 
                  str_extract("\\([:graph:]{1,}\\)") %>% str_remove_all("\\(|\\)") %>% as.Date(),
                url = rvest::html_nodes(html, css = "#docMore a") %>% as.character() %>% 
                  str_extract('(?<=href=")[:graph:]{1,}(?=\")')) %>% 
   mutate(url = url %>% 
-           str_replace(START %R% DOT, "http://www.china-embassy.org/chn/lszj/zytz")) %>% 
+           str_replace(START %R% DOT, "http://www.china-embassy.org/eng/notices/")) %>% 
   dplyr::arrange(desc(Time)) 
 
 content <- page$url %>% map_chr(extract_content)
 
 page_content_preview <- 
   page %>% 
-  mutate(Content_preview = paste0("[",content %>% str_remove_all("TRUE") %>% str_sub(1, 120), " ... (Read More)]","(", url,")")) %>% 
+  mutate(Content_preview = paste0("[",content %>% str_remove_all("TRUE") %>% str_sub(1, 300), " ... (Read More)]","(", url,")")) %>% 
   select(-url)
 
 header_1 <- 
@@ -41,5 +41,5 @@ header_2 <-
 table <- knitr::kable(page_content_preview , format = "pipe")
 
 output <- c(header_1, header_2, table)
-write(output, "README.md")
+write(output, "USA/README.md")
 
